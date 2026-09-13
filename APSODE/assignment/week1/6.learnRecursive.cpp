d94f5227ef48b2f6d6cb7389de00ee667eaccf45
APSODE <2gunbo74@gmail.com>
2026-09-13 15:21:32 +0900

    재귀함수에서 전달을 누락한 파라미터 수정

--- APSODE/assignment/week1/6.learnRecursive.cpp
+++ APSODE/assignment/week1/6.learnRecursive.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int print_reverse_digit(int value, int digit_base = 10) {
     if (value == 0) return 0;
     printf(" %d", value % digit_base);
-    return print_reverse_digit(value / digit_base);
+    return print_reverse_digit(value / digit_base, digit_base);
 }
 
 int print_digit(int value, int digit_base = 10) {
