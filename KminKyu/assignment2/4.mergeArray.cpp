c7c39fb4d3cc1f542b82e82070417fd4565b7b87
lhetl <93638355+lhetl@users.noreply.github.com>
2026-09-17 15:59:11 +0900

    :recycle: merge 수정

--- KminKyu/assignment2/4.mergeArray.cpp
+++ KminKyu/assignment2/4.mergeArray.cpp
@@ -1,3 +1,23 @@
+#include <iostream>
+using namespace std ;
+bool readAscSortedArray(int [], int) ;
+void mergeArray(int [], int [], int[], int) ;
+void printArray(int [], int) ;
+
+int main() {
+    const int SIZE = 5 ;
+    int cnt, arr1[SIZE], arr2[SIZE], resultArr[SIZE*2];
+    cin >> cnt ;
+    for (int i = 0 ; i < cnt ; ++i) {
+        if ( readAscSortedArray(arr1, SIZE) && readAscSortedArray(arr2, SIZE) ) {
+            mergeArray(arr1, arr2, resultArr, SIZE) ;
+            printArray(resultArr, SIZE*2) ;
+        }
+        else cout << "[Error] Unsorted input\n" ;
+    }
+    return 0 ;
+}
+
 bool readAscSortedArray(int arr[], int size) {
     for (int arrSize = 0; arrSize < size; ++arrSize) {
         cin >> arr[arrSize];
@@ -5,7 +25,7 @@ bool readAscSortedArray(int arr[], int size) {
     for (int arrSize = 0; arrSize < size - 1; ++arrSize) {
         if (arr[arrSize] > arr[arrSize + 1]) {
             return false;
-        }   
+        }
     }
     return true;
 }
