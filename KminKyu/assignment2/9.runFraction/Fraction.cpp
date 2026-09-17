92cf57574dbdd1620a3f49ca65a565d06c2f9b23
lhetl <hyun91000@gmail.com>
2026-09-18 00:55:04 +0900

    :bug: 오류 수정
    1. 음수에서도 작동
    2. swap 코드 수정
    3. 분수 정렬 수정
    4. add 로직 수정

--- KminKyu/assignment2/9.runFraction/Fraction.cpp
+++ KminKyu/assignment2/9.runFraction/Fraction.cpp
@@ -4,11 +4,11 @@
 using namespace std;
 
 int getGCD(int n, int d) {
-    if (d > n) {
+    if (d < n) {
         swap(d, n);
     }
     int temp;
-    while (n > 0) {
+    while (n != 0) {
         temp = n;
         n = d % n;
         d = temp;
@@ -16,8 +16,8 @@ int getGCD(int n, int d) {
     return d;
 }
 
-void swap(int* a, int* b) {
-    int* temp = a;
+void swap(int& a, int& b) {
+    int temp = a;
     a = b;
     b = temp;
 }
@@ -28,17 +28,18 @@ Fraction::Fraction(int n, int d)
 }
 
 void Fraction::organizeFraction() {
-    if (denominator < 0) {
-        denominator *= -1;
-        numerator *= -1;
-    } else if (denominator == 0) {
+    if (denominator == 0) {
         denominator = 1;
         cout << "ERR";
     }
     int gcd = getGCD(numerator, denominator);
-    if (gcd != 1) {
-        numerator /= gcd;
-        denominator /= gcd;
+    numerator /= gcd;
+    denominator /= gcd;
+
+    if (denominator < 0) {
+        denominator *= -1;
+        numerator *= -1;
+        return;
     }
 
 }
@@ -55,20 +56,16 @@ void Fraction::print() const {
     if (denominator != 1) {
         cout << "/" << denominator;
     }
+    cout << endl;
 }
 
 Fraction Fraction::add(Fraction fraction) {
-    int resultNumerator, resultDenominator;
-    int otherNumerator, otherDenominator;
+    int firstNumerator = this->getNumerator() * fraction.getDenominator();
+    int secondNumerator = fraction.getNumerator() * this->getDenominator();
 
-    otherNumerator = fraction.getNumerator();
-    otherDenominator = fraction.getDenominator();
+    int resultDenominator = fraction.getDenominator() * this->getDenominator();
 
-    int divNumDenominator = getGCD(otherNumerator, denominator);
-    int divNumNumerator = getGCD(numerator, otherDenominator);
-
-    resultNumerator = numerator / divNumNumerator + otherNumerator / divNumDenominator;
-    resultDenominator = denominator / divNumDenominator + otherDenominator / divNumNumerator;
+    int resultNumerator = firstNumerator + secondNumerator;
 
     return Fraction(resultNumerator, resultDenominator);
 }
--- KminKyu/assignment2/9.runFraction/main.cpp
+++ KminKyu/assignment2/9.runFraction/main.cpp
@@ -2,9 +2,9 @@
 #include "Fraction.h"
 int main()
 {
-    Fraction f1, f2(2, 5), f3 ; // f2 = 2/5
+    Fraction f1, f2(5, -4), f3 ; // f2 = 2/5
 
-    f1.set(2,3) ;  // f1 = 2/3
+    f1.set(2,6) ;  // f1 = 2/3
     f3 = f1.add(f2) ; // f1 = 2/3
     f3.print() ;
     return 0 ;
